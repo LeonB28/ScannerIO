41b69a3edaf9950477362cb620f3dda339b6373b
Leon <leon.bam@gmail.com>
2019-01-25 13:23:20 +0200

    change model

--- src/main/scala/org/scannerio/Entites/Entities.scala
+++ src/main/scala/org/scannerio/Entites/Entities.scala
@@ -1,5 +1,12 @@
 package org.scannerio.Entites
-case class Target(name: String)
+
+import scalaz.zio.IO
+
+case class Target(name: String, tasks: Seq[ScanTask])
 case class ScanTask(name: String)
 case class ScanTaskResult(content: List[String])
 case class ScanSubscriber(name: String)
+
+abstract class TaskAnalyze(name: String) {
+  def analyze(scanTaskResult: ScanTaskResult): IO[Exception, Boolean]
+}
